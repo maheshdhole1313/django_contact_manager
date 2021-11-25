13e5dfcebf923ed2437acae484282da13f456b24
Your Name <you@example.com>
2021-11-25 14:22:48 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from app.models import Contact
+from . import views
+urlpatterns = [
+    #path('',views.home,name="home"),
+    path('',views.HomePageView.as_view(),name="home"),
+
+    #path('detail/<int:id>/', views.detail, name="detail"),
+    path('detail/<int:pk>/', views.ContactDetailView.as_view(), name="detail"),
+    path('search/',views.search,name="search"),
+    path('contact/create',views.ContactCreateView.as_view(),name="create"),
+    path('contact/update/<int:pk>',views.ContactUpdateView.as_view(),name="update"),
+    path('contact/delete/<int:pk>',views.ContactDeleteView.as_view(),name="delete"),
+    path('signup/',views.SignUpView.as_view(),name="signup"),
+]
